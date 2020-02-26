3632f1b66d47dcb0cddc26d2d6cf7d37cc291c7f
SimonCanJer <SimonCanJer@gmail.com>
2020-02-26 11:50:36 +0200

    dummy

--- linux/gitHub.zuul.microhazel.lnx.sh
+++ linux/gitHub.zuul.microhazel.lnx.sh
@@ -3,6 +3,6 @@ mkdir     "$1"
 cd          "$1" || return
 git           clone https://github.com/SimonCanJer/zuul-hazel-route
 cd           zuul-hazel-route || return
-exec        install.linux.bat "$1"  "$2"
+exec        ./install.linux.sh "$1"  "$2"
 
 
