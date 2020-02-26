da9286870b37e24878d7e431559c1681a9a7117d
Simon <SimonCanJer@gmail.com>
2020-02-26 13:21:37 +0200

    improved install flow

--- linux/gitHub.spring.microhazel.lnx.sh
+++ linux/gitHub.spring.microhazel.lnx.sh
@@ -5,9 +5,11 @@ if [ $len -lt 2 ] ; then
 echo "wrong number of parameters, must be directories for spring micro and microjazel"
 return 
 fi
+echo "installing spring-micro-hazel in the $1"
 sudo mkdir "$1"
 cd     "$1" ||  return 
 git clone https://github.com/SimonCanJer/spring-micro-hazel.git
+echo "transferring control to spring-micro-hazel
 cd spring-micro-hazel
 cmod +x install.linux .sh
 ./install.linux .sh "$2"
--- linux/gitHub.zuul.microhazel.lnx.sh
+++ linux/gitHub.zuul.microhazel.lnx.sh
@@ -1,9 +1,12 @@
 mypath="$(pwd)"
-mkdir     "$1"
-cd          "$1" || return
-git           clone https://github.com/SimonCanJer/zuul-hazel-route
-cd           zuul-hazel-route || return
+echo "********installing zuul in the dir $1**************"
+wait 3
+mkdir   "$1"
+cd      "$1" || return
+git      clone https://github.com/SimonCanJer/zuul-hazel-route
+cd       zuul-hazel-route || return
+echo "transfering control to install of zull-hazel-route"
 chmod +x install.linux.sh
-./install.linux.sh "$1"  "$2"
+./install.linux.sh  "$2" "$3"
 
 
